011d75d57e50c971072da7ccfc1f7d4d96e23282
201220022 <1369268977@qq.com>
2026-02-23 06:04:50 -0800

    upd

---
+++ benchmarks/KCore/Hashbag/KCore.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <ParSet/ParSet.h>
+#include "hashbag.h"
+
+template <class Graph>
+parlay::sequence<uint32_t> KCore(Graph& G) {
+    const size_t n = G.n;
+    auto result = parlay::sequence<uint32_t>(n, 0);
+
+    auto active = ParSet::Active(n);
+    auto frontier = hashbag<uint32_t>(n);
+    auto D = parlay::tabulate<uint32_t>(n, [&](size_t s){ 
+        if (G.offsets[s+1] - G.offsets[s] == 0) { active.deactivate(s); }
+        return G.offsets[s+1] - G.offsets[s]; 
+    });
+
+    while (!active.empty()) {
+        uint32_t k = active.reduce_min(D);
+        active.parallel_do([&](uint32_t s) { 
+            if (D[s] <= k) { active.deactivate(s); frontier.insert(s);}
+        });
+        while (true) {
+            auto frt = frontier.pack();
+            if (frt.size() == 0) break;
+            parlay::parallel_for(0, frt.size(), [&](uint32_t i) {
+                uint32_t s = frt[i];
+                result[s] = k;
+                ParSet::adaptive_for(G.offsets[s], G.offsets[s + 1], [&](size_t j) {
+                    uint32_t d = G.edges[j].v;
+                    if (active.is_active(d)) {
+                        if (__atomic_fetch_sub(&D[d], 1, __ATOMIC_RELAXED) <= k+1) {
+                            active.deactivate(d); frontier.insert(d);
+                        }
+                    }
+                });
+            });
+        }
+    }
+    
+    return result;
+}
---
+++ benchmarks/KCore/Pack/KCore.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <ParSet/ParSet.h>
+
+template <class Graph>
+parlay::sequence<uint32_t> KCore(Graph& G) {
+    const size_t n = G.n;
+    auto result = parlay::sequence<uint32_t>(n, 0);
+
+    auto active = ParSet::Active(n);
+    auto frontier = ParSet::Frontier(n);
+    auto frt = parlay::sequence<uint32_t>(n, 0);
+    auto D = parlay::tabulate<uint32_t>(n, [&](size_t s){ 
+        if (G.offsets[s+1] - G.offsets[s] == 0) { active.deactivate(s); }
+        return G.offsets[s+1] - G.offsets[s]; 
+    });
+
+    while (!active.empty()) {
+        uint32_t k = active.reduce_min(D);
+        active.parallel_do([&](uint32_t s) { 
+            if (D[s] <= k) { active.deactivate(s); frontier.insert(s);}
+        });
+        while (frontier.advance()) {
+            size_t frt_size = frontier.pack(frt);
+            parlay::parallel_for(0, frt_size, [&](uint32_t i) {
+                uint32_t s = frt[i];
+                result[s] = k;
+                ParSet::adaptive_for(G.offsets[s], G.offsets[s + 1], [&](size_t j) {
+                    uint32_t d = G.edges[j].v;
+                    if (active.is_active(d)) {
+                        if (__atomic_fetch_sub(&D[d], 1, __ATOMIC_RELAXED) <= k+1) {
+                            active.deactivate(d); frontier.insert(d);
+                        }
+                    }
+                });
+            });
+        }
+    }
+    
+    return result;
+}
