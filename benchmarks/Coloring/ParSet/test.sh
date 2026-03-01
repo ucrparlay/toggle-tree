c6893c4066d0a11ca83e2b6f086fdf758ca2505b
Xinjian Jiang <xjian140@xe-15.cs.ucr.edu>
2026-03-01 14:22:01 -0800

    upd

--- benchmarks/Coloring/ParSet/test.sh
+++ benchmarks/Coloring/ParSet/test.sh
@@ -1,6 +1,5 @@
 make clean
 make
 numactl -i all ./test ../../../benchmark_utils/HepPh_sym.bin
-source ../../../benchmark_utils/config.sh
+#source ../../../benchmark_utils/config.sh
 #numactl -i all ./test "${BIN_DIR}planet_sym.bin"
-numactl -i all ./test "${BIN_DIR}twitter_sym.bin"
