1f367b1a306699fe8d6f5d9d260fed102f344359
Xinjian Jiang <xjian140@xe-15.cs.ucr.edu>
2026-03-03 13:18:36 -0800

    upd

--- benchmarks/KCore/ParSet/test.sh
+++ benchmarks/KCore/ParSet/test.sh
@@ -1,5 +1,5 @@
 make clean
 make
 numactl -i all ./test ../../../benchmark_utils/HepPh_sym.bin
-#source ../../../benchmark_utils/scripts/config.sh
+#source ../../../benchmark_utils/config.sh
 #numactl -i all ./test "${BIN_DIR}planet_sym.bin"
