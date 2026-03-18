b08a773260da6a8b82fd2a0f821f535dc84bb7a6
alanSquirrelyz <yliu908@ucr.edu>
2026-03-18 10:40:25 -0700

    tmp

--- benchmark_utils/scripts/bench_supp.sh
+++ benchmark_utils/scripts/bench_supp.sh
@@ -5,10 +5,11 @@ rm -f BFS/*.csv
 rm -f KCore/*.csv
 rm -f ColoringRound/*.csv
 
-cd BFS/Hashbag && bash ./bench.sh && cd ../..
+# cd BFS/Hashbag && bash ./bench.sh && cd ../..
 
 # cd KCore/Hashbag && bash ./bench.sh && cd ../..
-cd KCore/HashbagKCore && bash ./bench.sh && cd ../..
+# cd KCore/HashbagKCore && bash ./bench.sh && cd ../..
 cd KCore/HashbagSampling && bash ./bench.sh && cd ../..
+cd KCore/ParSetSampling && bash ./bench.sh && cd ../..
 
 # cd ColoringRound/Hashbag && bash ./bench.sh && cd ../..
