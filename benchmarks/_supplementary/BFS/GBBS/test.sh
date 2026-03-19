fb73f2bf5dfd5e941692019113d977df2ea637af
Xinjian Jiang <1369268977@qq.com>
2026-03-19 03:19:36 -0700

    upd

--- benchmarks/_supplementary/BFS/GBBS/test.sh
+++
@@ -1,5 +0,0 @@
-cd ../../../../benchmark_utils/bazel
-bazel build @gbbs_sparsebfs//:BFS_main -c opt
-numactl -i all bazel-bin/external/gbbs_sparsebfs/BFS_main -s -b ../HepPh_sym.bin
-#source ../scripts/config.sh
-#numactl -i all bazel-bin/external/gbbs_sparsebfs/BFS_main -s -b "${BIN_DIR}planet_sym.bin"
