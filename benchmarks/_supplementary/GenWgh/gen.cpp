2a901a683cb5184ec6d18386061f1b697b11e6aa
Xinjian Jiang <1369268977@qq.com>
2026-03-23 19:52:03 -0700

    upd

--- benchmarks/_supplementary/GenWgh/gen.cpp
+++ benchmarks/_supplementary/GenWgh/gen.cpp
@@ -12,7 +12,8 @@ int main(int argc, char** argv) {
     if (name.find("_wgh") != std::string::npos) { name = name.substr(0, name.find("_wgh")); }
     if (G.symmetrized) name += "_sym";
     if (G.weighted) name += "_wgh";
-    if (std::string(range) != "0") name += range;
+    if (std::string(range) != "0") { name += range; }
+    else { name += "log"; }
     name += ".adj";
     std::cerr << "New graph name = " << name << "\n";
     G.write_pbbs_format(name.c_str());
