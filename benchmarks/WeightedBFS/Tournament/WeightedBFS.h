7f3b4eb0251951fbd43688e0cc906837710eea34
Xinjian Jiang <1369268977@qq.com>
2026-03-26 20:58:38 -0700

    upd

--- benchmarks/WeightedBFS/Tournament/WeightedBFS.h
+++ benchmarks/WeightedBFS/Tournament/WeightedBFS.h
@@ -8,7 +8,7 @@ static inline bool write_min(int32_t& ref, int32_t v){
 }
 
 template <class Graph>
-parlay::sequence<int32_t> Dijkstra(Graph& G, size_t source=0) {
+parlay::sequence<int32_t> WeightedBFS(Graph& G, size_t source=0) {
     const size_t n = G.n;
     auto active = ParSet::Active(n, false); 
     auto frontier = ParSet::Frontier(n);
@@ -16,16 +16,11 @@ parlay::sequence<int32_t> Dijkstra(Graph& G, size_t source=0) {
     active.insert(source); frontier.insert_next(source);
     parlay::internal::timer t; double t1=0,t2=0,t3=0;
     for (uint32_t round = 0;  ;round++) {
-        if(active.empty()) { std::cerr << "round=" << round << "\n"; break; }
-        t.start();
+        if(active.empty()) { break; }
         frontier.advance_to_next();
         frontier.repair(active, dist);
-        t1 += t.stop();
-        t.start();
         active.extract_min(frontier, dist);
-        t2 += t.stop();
-        t.start();
-        if(!frontier.advance_to_next()) { std::cerr << "round=" << round << "\n"; break; }
+        frontier.advance_to_next();
         frontier.for_each([&](uint32_t s) { 
             int32_t dist_s = dist[s];
             ParSet::adaptive_for(G.offsets[s], G.offsets[s + 1], [&](size_t i) {
@@ -38,8 +33,5 @@ parlay::sequence<int32_t> Dijkstra(Graph& G, size_t source=0) {
         });
         t3 += t.stop();
     }
-    std::cerr << "\nt1 = " << t1 << "\n";
-    std::cerr << "t2 = " << t2 << "\n";
-    std::cerr << "t3 = " << t3 << "\n";
     return dist;
 }
